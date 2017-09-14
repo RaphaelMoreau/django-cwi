70f9a47830a129c1e2d6fd15fa8655a1f37d1967
Raphaël Moreau <raf.moreau@gmail.com>
2017-09-14 11:23:52 +0200

    Created model for application, including administration site and migrations. Very basic views and template for displaying model and adding. Updated templating hierarchy to add page title

--- www/models.py
+++ www/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+# Application
+class Application(models.Model):
+    name = models.CharField(max_length=50)
+    cryptoKey = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
 # Create your models here.
--- www/views.py
+++ www/views.py
@@ -2,6 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
+from .models import Application
+
 @login_required()
 def index(request):
-    return render(request, 'www/index.html')
+    apps = Application.objects.all()
+    context = {
+        'apps': apps
+    }
+    return render(request, 'www/index.html', context)
+
+def appDetail(request, pk):
+    return HttpResponse("Display application "+pk)
+
+def appCreate(request):
+    return HttpResponse("Creating a new application")
