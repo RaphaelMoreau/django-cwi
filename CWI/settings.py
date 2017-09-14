f43754b7aea313ea7039ee96132f5ca2b8a3a97e
Raphaël Moreau <raf.moreau@gmail.com>
2017-09-14 08:02:08 +0200

    Templating hierarchy setup. Login and logout system setup

--- CWI/settings.py
+++ CWI/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'www',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# The login url
+LOGIN_URL = '/login'
+
+# where to redirect after login
+LOGIN_REDIRECT_URL = '/'
+
+# where to redirect after logout
+LOGOUT_REDIRECT_VIEW = '/'
--- www/urls.py
+++ www/urls.py
@@ -1,7 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url,include
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='www/login.html'), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(template_name='www/logged_out.html'), name='logout'),
     url(r'^$', views.index, name='index'),
 ]
