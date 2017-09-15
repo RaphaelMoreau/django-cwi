5e10e5bf8da86f723f9ad70b5a6199367545d42a
Raphaël Moreau <raf.moreau@gmail.com>
2017-09-15 13:50:19 +0200

    Created Country model (with admin) and code to import data

--- www/admin.py
+++ www/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Application
+from .models import Application, Country
 
 # Register your models here.
 admin.site.register(Application)
+admin.site.register(Country)
--- www/models.py
+++ www/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+#Reference tables
+class Country(models.Model):
+    codeA2 = models.CharField("ISO Alpha2 code",max_length=2, primary_key=True)
+    codeA3 = models.CharField("ISO Alpha3 code",max_length=3)
+    name = models.CharField("Country name",max_length=100)
+
+    def __str__(self):
+        return "%s (%s)" % (self.name,self.codeA2)
+
 # Application
 class Application(models.Model):
     name = models.CharField(max_length=50, unique=True)
