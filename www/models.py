e4a6b24cf6d4cde699e1380e4ac6b8255befc079
Raphaël Moreau <raf.moreau@gmail.com>
2017-09-15 08:03:25 +0200

    Added unique constraint on application name

--- www/models.py
+++ www/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Application
 class Application(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     cryptoKey = models.CharField(max_length=200)
 
     def __str__(self):
