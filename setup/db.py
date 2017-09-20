5c627717bc5bc05f662b21ea7da4acb87cd8faef
Raphaël Moreau <raf.moreau@gmail.com>
2017-09-20 22:41:35 +0200

    Created setup module to initialize database

---
+++ setup/db.py
@@ -0,0 +1,41 @@
+import csv
+from www.models import Country, AdType, AdPlace
+import sys,os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def countries():
+    print("Importing country codes")
+    file=open(os.path.join(BASE_DIR,'CountryCodes.csv'))
+    reader=csv.reader(file)
+    first=True
+    for p in reader:
+        if first:
+            first=False
+        else:
+            c=Country(name=p[0], codeA2=p[1], codeA3=p[2], display=p[4])
+            c.save()
+
+def types():
+    print("Importing ad types")
+    file=open(os.path.join(BASE_DIR,'AdTypes.csv'))
+    reader=csv.reader(file)
+    first=True
+    for p in reader:
+        if first:
+            first=False
+        else:
+            c=AdType(id=p[0], name=p[1])
+            c.save()
+
+def places():
+    print("Importing ad places")
+    file=open(os.path.join(BASE_DIR,'AdPlaces.csv'))
+    reader=csv.reader(file)
+    first=True
+    for p in reader:
+        if first:
+            first=False
+        else:
+            c=AdPlace(id=p[0], name=p[1])
+            c.save()
