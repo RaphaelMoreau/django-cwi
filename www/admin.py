fd2498a948994ff724b9eadba6e200a8cb7d862c
Raphaël Moreau <raf.moreau@gmail.com>
2017-09-22 10:41:08 +0200

    Defined admin site various titles

--- www/admin.py
+++ www/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 
 from .models import Application, Country, ApplicationAdConfig, Platform, AdType, AdPlace
 
-# Register your models here.
+#Defining admin site titles
+admin.site.site_header = "CWI Administration"
+admin.site.site_title = "CWI Administration site"
+admin.site.index_title = "CWI Administration"
+
+# Registering models
 admin.site.register(Application)
 admin.site.register(Country)
 admin.site.register(ApplicationAdConfig)
