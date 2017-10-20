6eb6d5fae0a608b0fc5def0aa61129ba2563b02f
Raphaël Moreau <raf.moreau@gmail.com>
2017-10-20 18:47:11 +0200

    Added model for AdType parameters with admin

--- www/admin.py
+++ www/admin.py
@@ -7,13 +7,17 @@ admin.site.site_header = "CWI Administration"
 admin.site.site_title = "CWI Administration site"
 admin.site.index_title = "CWI Administration"
 
+# registering reference tables for admin
+admin.site.register(Country)
+admin.site.register(Platform)
+admin.site.register(AdType)
+admin.site.register(AdTypeParameter)
+admin.site.register(AdPlace)
+
 # Registering models
 admin.site.register(Application)
 admin.site.register(ApplicationCountry)
 admin.site.register(ApplicationPlatform)
 admin.site.register(ApplicationAdType)
+admin.site.register(ApplicationAdTypeParameter)
 admin.site.register(ApplicationAdPlace)
-admin.site.register(Country)
-admin.site.register(Platform)
-admin.site.register(AdType)
-admin.site.register(AdPlace)
--- www/models.py
+++ www/models.py
@@ -67,6 +67,10 @@ class ApplicationAdType(models.Model):
     def __str__(self):
         return "%s-%s" % (self.platform, self.adType)
 
+    def parameters(self):
+        p = ApplicationAdTypeParameter.objects.filter(applicationAdType=self.id)
+        return AdTypeParameter.objects.filter(adType=self.adType).prefetch_related(models.Prefetch("applicationadtypeparameter_set", queryset=p, to_attr='value'))
+
 # application ad place, linked to an application platform and an application ad type
 class ApplicationAdPlace(models.Model):
     platform = models.ForeignKey(ApplicationPlatform, on_delete=models.CASCADE)
@@ -77,3 +81,131 @@ class ApplicationAdPlace(models.Model):
 
     def __str__(self):
         return "%s-%s-%s" % (self.platform, self.adPlace, self.adType.adType)
+
+#utility str to bool conversion function - private
+def _str_to_bool(value):
+    return value == 'True'
+
+#Abstract class for parameters
+class Parameter(models.Model):
+    INTEGER = 1
+    STRING = 2
+    BOOLEAN = 3
+    PERCENTAGE = 4
+    TYPES_CHOICES = (
+        (INTEGER, 'Integer'),
+        (STRING, 'String'),
+        (BOOLEAN, 'Boolean'),
+        (PERCENTAGE, 'Percentage')
+    )
+    CONVERSIONS={
+        INTEGER: ('Integer', int),
+        STRING: ('String', str),
+        BOOLEAN: ('Boolean', _str_to_bool),
+        PERCENTAGE: ('Percentage', int),
+    }
+    name = models.CharField(max_length=45)
+    type = models.IntegerField(choices=TYPES_CHOICES, default=INTEGER)
+    mandatory = models.BooleanField()
+    editable = models.BooleanField(default=True)
+    default = models.CharField(max_length=100)
+    helpText = models.CharField(max_length=200, null=True, blank=True)
+
+    def __init__(self, *args, **kwargs):
+        super(Parameter, self).__init__(*args, **kwargs)
+        self.type_text, self.convert = Parameter.CONVERSIONS.get(self.type)
+
+    def effective_value(self):
+        if self.editable:
+            return self.convert(self.value[0].value if self.value else self.default)
+        return self.convert(self.value[0].value if self.value else self.default)
+
+    class Meta:
+        abstract = True
+
+# Ad types parameters
+class AdTypeParameter(Parameter):
+    adType = models.ForeignKey(AdType, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return "%s-%s" % (self.adType, self.name)
+
+    def _Update(self, value, applicationAdType):
+        self.value[0].value = value
+        self.value[0].save()
+        return True
+
+    def _Create(self, value, applicationAdType):
+        p = ApplicationAdTypeParameter.objects.create(applicationAdType=applicationAdType, parameter=self, value=value)
+        return True
+
+    def _Remove(self, value, applicationAdType):
+        self.value[0].delete()
+        return True
+
+    def _Ignore(self, value, applicationAdType):
+        return False
+
+    def store(self, value, applicationAdType):
+        'Save value for parameter. Return True if changed, False otherwise'
+        # the actions to perform depending on various conditions
+        _actions = {
+            'MEUN': self._Ignore,
+            'MEUD': self._Ignore, #Do not store mandatory value same as default
+            'MEUS': self._Ignore, #nonsense
+            'MEUO': self._Create, #Create sub value
+            'MESN': self._Remove, #Reset to default value by removing sub value
+            'MESD': self._Remove, #Reset to default value by removing sub value
+            'MESS': self._Ignore,
+            'MESO': self._Update,
+            'MFUN': self._Ignore,
+            'MFUD': self._Ignore,
+            'MFUS': self._Ignore, #nonsense
+            'MFUO': self._Ignore, #That case should not happen. Just ignore it
+            'MFSN': self._Remove, #No sub value should be stored on mandatory non editable parameters. So remove it
+            'MFSD': self._Remove, #No sub value should be stored on mandatory non editable parameters. So remove it
+            'MFSS': self._Remove, #No sub value should be stored on mandatory non editable parameters. So remove it
+            'MFSO': self._Remove, #No sub value should be stored on mandatory non editable parameters. So remove it
+            'OEUN': self._Ignore,
+            'OEUD': self._Create,
+            'OEUS': self._Ignore, #nonsense
+            'OEUO': self._Create,
+            'OESN': self._Remove,
+            'OESD': self._Update,
+            'OESS': self._Ignore,
+            'OESO': self._Update,
+            'OFUN': self._Ignore,
+            'OFUD': self._Create,
+            'OFUS': self._Create,
+            'OFUO': self._Create,
+            'OFSN': self._Remove,
+            'OFSD': self._Update,
+            'OFSS': self._Ignore,
+            'OFSO': self._Update,
+        }
+        # get converted stored value and default value
+        stored_value = self.convert(self.value[0].value) if self.value else None
+        default = self.convert(self.default)
+        # build a string to find the action to perform
+        s = ('M' if self.mandatory else 'O')  # M if mandatory, O if optional
+        s += ('E' if self.editable else 'F')  # E if editable, F if fixed
+        s += ('S' if stored_value is not None else 'U')     # S if value is set, U if unset
+        if value is None: # N if no value provided
+            s += 'N'
+        elif value == default: # D if provided value is same as default
+            s += 'D'
+        elif self.value and value == stored_value: # S if provided value is same as stored value if it exists
+            s += 'S'
+        else: # all other cases, i.e. provided value is different of default and different of stored value (if it exists)
+            s += 'O'
+        # Perform the action and return its value
+        return _actions[s](value, applicationAdType)
+
+
+class ApplicationAdTypeParameter(models.Model):
+    applicationAdType = models.ForeignKey(ApplicationAdType)
+    parameter = models.ForeignKey(AdTypeParameter)
+    value = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return "%s-%s" % (self.applicationAdType, self.parameter)
